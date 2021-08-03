bbdb7ea565f5cec49caa63f76392ce31788a3304
Crowdbotics <team@crowdbotics.com>
2021-08-03 23:29:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "billowing_cell_29432Identifier",
+  url: "https://billowing_cell_29432Identifier.botics.co",
+  api: "https://billowing_cell_29432Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
